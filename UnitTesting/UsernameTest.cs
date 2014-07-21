9800e763bab38101d1daaaed83346e4c6bc00ede
Justin <jmorgan@accesssoftek.com>
2014-07-21 12:43:34 -0700

    Initial commit

---
+++ UnitTesting/UsernameTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using Selenium;
+
+namespace UnitTesting
+{
+    [TestFixture]
+    public class UsernameTest
+    {
+        private IWebDriver driver;
+        private String urlPrefix = "qavip";
+        private int waitTime = 3000;
+        private const String user = "admin";
+        private const String password = "1234";
+        //private Boolean test = false;
+
+        [Test]
+        public void Init()
+        {
+            driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://" + urlPrefix + ".orpheusdev.net/");
+            Console.WriteLine(driver.FindElement(By.Name("version")).GetAttribute("content"));
+
+            IWebElement userName = driver.FindElement(By.Name("UserName"));
+            Assert.IsTrue(userName.Displayed);
+            userName.SendKeys(user);
+            userName.SendKeys(Keys.Enter);
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(waitTime));
+
+            IWebElement personalImage = driver.FindElement(By.XPath("/html/body/div/div[1]/div[2]/div/div/div/div/div/div[1]/img"));
+            Assert.IsTrue(personalImage.Displayed);
+
+            IWebElement personalPhrase = driver.FindElement(By.XPath("/html/body/div/div[1]/div[2]/div/div/div/div/div/div[1]/p/img"));
+            Assert.IsTrue(personalPhrase.Displayed);
+        }
+    }
+}
